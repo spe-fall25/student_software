1fd3594a9d56d5adfe98f24109ae89d3f664120f
Xuhao Chen <cxh.bass@gmail.com>
2024-12-22 12:46:07 -0500

    Update install_opencilk.sh

--- scripts/install_opencilk.sh
+++ scripts/install_opencilk.sh
@@ -6,9 +6,9 @@ if [[ $EUID -ne 0 ]]; then
 else
   opencilk_url="http://6.172.scripts.mit.edu/opencilk_6106.tar.gz"
   echo "Installing OpenCilk"
-  mkdir -p /opt/6106/opencilk
-  rm -rf /opt/6106/opencilk/*
-  curl -L $opencilk_url | tar -zxv --strip-components=1 -C /opt/6106/opencilk
+  mkdir -p /opt/spe/opencilk
+  rm -rf /opt/spe/opencilk/*
+  curl -L $opencilk_url | tar -zxv --strip-components=1 -C /opt/spe/opencilk
 
-  ln -sf /opt/6106/opencilk/bin/clang /usr/bin/clang-6106
+  ln -sf /opt/spe/opencilk/bin/clang /usr/bin/clang-spe
 fi
