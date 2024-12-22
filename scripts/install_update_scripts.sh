9fd1cb91a0ccd9ccd994d202b6b607e776041dc2
Xuhao Chen <cxh.bass@gmail.com>
2024-12-22 12:47:30 -0500

    Update install_update_scripts.sh

--- scripts/install_update_scripts.sh
+++ scripts/install_update_scripts.sh
@@ -4,17 +4,17 @@ set -e
 if [[ $EUID -ne 0 ]]; then
    sudo -E $0
 else
-   mkdir -p /opt/6106
+   mkdir -p /opt/spe
 
-   echo "Creating symlink update-clang-6106"
-   cp scripts/install_opencilk.sh /opt/6106/update-clang-6106.sh
-   ln -sf /opt/6106/update-clang-6106.sh /usr/bin/update-clang-6106
+   echo "Creating symlink update-clang-spe"
+   cp scripts/install_opencilk.sh /opt/spe/update-clang-spe.sh
+   ln -sf /opt/spe/update-clang-spe.sh /usr/bin/update-clang-spe
 
    echo "Creating symlink update-telerun"
-   cp scripts/install_telerun.sh /opt/6106/update-telerun.sh
-   ln -sf /opt/6106/update-telerun.sh /usr/bin/update-telerun
+   cp scripts/install_telerun.sh /opt/spe/update-telerun.sh
+   ln -sf /opt/spe/update-telerun.sh /usr/bin/update-telerun
 
    echo "Creating symlink authorize-telerun"
-   cp scripts/authorize_telerun.sh /opt/6106/authorize-telerun.sh
-   ln -sf /opt/6106/authorize-telerun.sh /usr/bin/authorize-telerun
+   cp scripts/authorize_telerun.sh /opt/spe/authorize-telerun.sh
+   ln -sf /opt/spe/authorize-telerun.sh /usr/bin/authorize-telerun
 fi
