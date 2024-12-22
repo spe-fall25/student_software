0d4466ca044f8fc840f6823eb621365dfa3a7c65
Xuhao Chen <cxh.bass@gmail.com>
2024-12-22 11:32:50 -0500

    Update install_telerun_alt.sh

--- scripts/install_telerun_alt.sh
+++ scripts/install_telerun_alt.sh
@@ -4,7 +4,7 @@ set -e
 if [[ $EUID -ne 0 ]]; then
    sudo -E $0
 else
-   telerun_url="https://raw.githubusercontent.com/6106-fall24/student_software/main/telerun/submit_alt.py"
+   telerun_url="https://raw.githubusercontent.com/CSE491-spring25/student_software/main/telerun/submit_alt.py"
    echo "Installing telerun"
    mkdir -p /opt/6106
    curl -O --output-dir /opt/6106 $telerun_url
